3a30aa3bada1f58a69deed093c3b7c74ed922b29
Qwerty1151 <you@example.com>
2025-10-21 15:44:03 +0300

    fix(cms): add api functions + package.json + vercel.json (KV, Node18)

--- api/content-get.js
+++ api/content-get.js
@@ -1,5 +1,4 @@
 const { kv } = require("@vercel/kv");
-
 module.exports = async (req, res) => {
   try {
     const url = new URL(req.url, `http://${req.headers.host}`);
@@ -11,6 +10,7 @@ module.exports = async (req, res) => {
     res.end(JSON.stringify({ ok: true, key, value: value ?? null }));
   } catch (e) {
     res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.end(JSON.stringify({ ok: false, error: String(e && e.message || e) }));
   }
 };
